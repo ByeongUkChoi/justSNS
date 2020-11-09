1146daf9fc0f88e42ffe194d518d063c3071c9fd
ByeongUk Choi <cbw0916@gmail.com>
2020-11-09 15:18:01 +0900

    first commit

---
+++ src/main/java/com/byeongukchoi/justSNS/JustSnsApplication.java
@@ -0,0 +1,13 @@
+package com.byeongukchoi.justSNS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byeongukchoi/justSNS/JustSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byeongukchoi.justSNS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
