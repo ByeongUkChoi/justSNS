8e16e485e74963edcf298f344a1f567d72f2f243
ByeongUk Choi <cbw0916@gmail.com>
2020-11-17 06:25:07 +0900

    Remove unused code

--- src/main/java/com/byeongukchoi/justSNS/controller/AuthController.java
+++ src/main/java/com/byeongukchoi/justSNS/controller/AuthController.java
@@ -1,16 +1,11 @@
 package com.byeongukchoi.justSNS.controller;
 
 import com.byeongukchoi.justSNS.dto.UserDto;
-import com.byeongukchoi.justSNS.model.User;
 import com.byeongukchoi.justSNS.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
