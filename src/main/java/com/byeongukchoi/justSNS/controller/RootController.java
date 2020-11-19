198075b5378991421881ff5fd54950a70bf95224
ByeongUk Choi <cbw0916@gmail.com>
2020-11-19 21:00:28 +0900

    Add Hateoas root controller

---
+++ src/main/java/com/byeongukchoi/justSNS/controller/RootController.java
@@ -0,0 +1,22 @@
+package com.byeongukchoi.justSNS.controller;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public RepresentationModel root() {
+
+        RepresentationModel model = new RepresentationModel();
+
+        model.add(linkTo(methodOn(RootController.class).root()).withSelfRel());
+        model.add(linkTo(methodOn(PostController.class).getPosts(0, 20)).withRel("getPosts"));
+
+        return model;
+    }
+}
