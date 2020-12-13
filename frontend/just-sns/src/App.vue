b4deb4d63bb0861ac9ff237dcd27774b003d0732
ByeongUk Choi <cbw0916@gmail.com>
2020-12-14 07:29:39 +0900

    Add Header

--- frontend/just-sns/src/App.vue
+++ frontend/just-sns/src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header></app-header>
+    <b-container>
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </b-container>
   </div>
 </template>
 
 <script>
+import AppHeader from '@/components/AppHeader';
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
+    AppHeader,
     HelloWorld
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ frontend/just-sns/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav>
+      <b-nav-item href="#">Home</b-nav-item>
+    </b-navbar-nav>
+
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item-dropdown text="User" right>
+        <b-dropdown-item href="#">Account</b-dropdown-item>
+        <b-dropdown-item href="#">Settings</b-dropdown-item>
+      </b-nav-item-dropdown>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
