5dacf086ab490fc5fa2d2507080b3fe6025ab05a
ByeongUk Choi <cbw0916@gmail.com>
2020-12-17 17:37:38 +0900

    Add post loop

--- frontend/just-sns/src/components/PostList.vue
+++ frontend/just-sns/src/components/PostList.vue
@@ -1,12 +1,48 @@
 <template>
-<div>
-    Hello world
-</div>
+  <div>
+    <div v-if="isLoading" >Loading posts...</div>
+    <div v-else>
+      <div v-if="posts.length === 0"> No posts are here... yet.</div>
+      <div v-for="post in posts" v-bind:key="post.id">
+        {{post.subject}}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            posts: [],
+            isLoading: true
+        }
+    },
+    mounted() {
+        this.fetchPosts();
+    },
+    computed: {
+    },
+    methods: {
+        fetchPosts() {
+            if (this.isLoading) {
+                this.isLoading = false;
+            }
+            // TODO: test
+            this.posts = [
+                {
+                    id: 1,
+                    subject: "test 1",
+                    author: "T"
+                },
+                {
+                    id: 2,
+                    subject: "test 2",
+                    author: "ABC"
+                }
+            ]
+        }
+    }
 }
 </script>
 
