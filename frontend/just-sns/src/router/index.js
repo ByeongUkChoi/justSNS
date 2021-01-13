4bc8a3f0c70f9f2191943dbe2834660743d9066b
ByeongUk Choi <cbw0916@gmail.com>
2021-01-13 22:01:02 +0900

    Add sign-up router

--- frontend/just-sns/src/router/index.js
+++ frontend/just-sns/src/router/index.js
@@ -4,11 +4,17 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 export default new VueRouter({
+    mode: 'history',
     routes: [
         {
             path: '/',
             name: 'home',
             component: () => import("@/views/Home"),
+        },
+        {
+            path: '/sign-up',
+            name: 'signUp',
+            component: () => import("@/views/SignUp"),
         },
         {
             path: '/write',
