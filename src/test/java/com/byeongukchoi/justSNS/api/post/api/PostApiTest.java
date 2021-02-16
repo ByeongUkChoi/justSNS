08554168275bbb81f3e36ceca62a51c50e60534e
ByeongUk Choi <cbw0916@gmail.com>
2021-02-17 06:57:45 +0900

    Fix api test

--- src/test/java/com/byeongukchoi/justSNS/api/post/api/PostApiTest.java
+++ src/test/java/com/byeongukchoi/justSNS/api/post/api/PostApiTest.java
@@ -13,15 +13,19 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser
 public class PostApiTest {
     @Autowired
     private MockMvc mockMvc;
@@ -30,18 +34,23 @@ public class PostApiTest {
     private PostService postService;
 
     @Test
-    @WithMockUser
     public void getPost() throws Exception {
-        // TODO: given 데이터 추가
+        // given
+        PostDto.Response postResponseDto = mock(PostDto.Response.class);
+        setField(postResponseDto, "id", 1L);
+        setField(postResponseDto, "content", "This is content");
+        setField(postResponseDto, "createdAuthorId", 1L);
+        setField(postResponseDto, "createdAt", ZonedDateTime.now());
+        setField(postResponseDto, "updatedAt", ZonedDateTime.now());
+
         List<PostDto.Response> list = new ArrayList<>();
-        PagedResponse<PostDto.Response> pagedResponse = new PagedResponse<>(list, 1, 2, 3);
-        given(postService.getPosts(eq(0), eq(20)))
+        PagedResponse<PostDto.Response> pagedResponse = new PagedResponse<>(list, 0, 20, list.size());
+        given(postService.getPosts(anyInt(), anyInt()))
                 .willReturn(pagedResponse);
 
         // TODO: when. 내용 추가
         ResultActions result = mockMvc.perform(
                 get("/posts")
-                        .content("abc")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
         );
